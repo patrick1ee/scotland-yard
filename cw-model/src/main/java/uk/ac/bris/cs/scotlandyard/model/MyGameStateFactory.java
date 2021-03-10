1bf9e5e2e0fbbaf268032570657d6cba29b26cf2
an20421 <an20421@bristol.ac.uk>
2021-03-10 15:34:30 +0000

    MyGameSateFactory

--- cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -4,13 +4,12 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * cw-model
@@ -19,6 +18,20 @@ import java.util.Optional;
 public final class MyGameStateFactory implements Factory<GameState> {
 
 	private final class MyGameState implements GameState {
+
+		private final class MyTicketBoard implements TicketBoard{
+
+			private final ImmutableMap<ScotlandYard.Ticket, Integer> tickets;
+
+			@Override
+			public int getCount(@Nonnull ScotlandYard.Ticket ticket) {
+				return this.tickets.getOrDefault(ticket, 0);
+			}
+
+			private MyTicketBoard(ImmutableMap<ScotlandYard.Ticket, Integer> tickets){
+				this.tickets = tickets;
+			}
+		}
 
 		private GameSetup setup;
 		private ImmutableSet<Piece> remaining;
@@ -31,12 +44,39 @@ public final class MyGameStateFactory implements Factory<GameState> {
 
 		@Override public GameSetup getSetup() {  return this.setup; }
 		@Override public ImmutableSet<Piece> getPlayers() { return this.remaining; }
-		@Override public Optional<Integer> getDetectiveLocation(Piece.Detective detective) { return null; }
-		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) { return null; }
+
+		@Override public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+			for (Player p : this.detectives){
+				if(p.piece() == detective) return Optional.of(p.location());
+			}
+			return Optional.empty();
+		}
+
+		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			for (Player p : this.everyone){
+				if(p.piece() == piece) return Optional.of(new MyTicketBoard(p.tickets()));
+			}
+			return Optional.empty();
+		}
+
 		@Override public ImmutableList<LogEntry> getMrXTravelLog() { return this.log; }
-		@Override public ImmutableSet<Piece> getWinner() { return null; }
+		@Override public ImmutableSet<Piece> getWinner() { return this.winner; }
 		@Override public ImmutableSet<Move> getAvailableMoves() { return this.moves; }
 		@Override public GameState advance(Move move) {  return null;  }
+
+		private boolean verifyDetectives(List<Player> detectives){
+			Set<Integer> locs = new HashSet<>();
+			Set<String> cols = new HashSet<>();
+			for(Player p : detectives){
+				if(!p.piece().isDetective()) return false;
+				if(p.piece().webColour() == "#000") return false;
+				locs.add(p.location());
+				cols.add(p.piece().webColour());
+			}
+			return cols.size() == detectives.size() && locs.size() == detectives.size();
+
+
+		}
 
 		private MyGameState(
 				final GameSetup setup,
@@ -50,6 +90,10 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			this.log = Objects.requireNonNull(log, "log must not be null");
 			this.mrX = Objects.requireNonNull(mrX, "mrX must not be null");
 			this.detectives = Objects.requireNonNull(detectives, "detectives must not be null");
+
+			if(this.setup.rounds.isEmpty()) throw new IllegalArgumentException("Rounds is empty");
+			if(verifyDetectives(this.detectives)) throw new IllegalArgumentException("Invalid detectives");
+			if(this.mrX.piece().webColour() != "#000") throw new IllegalArgumentException("MrX is not the black piece");
 		}
 	}
 
